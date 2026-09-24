ff208ecde698e6551b791207c35c818b0bd7be59
Mohammed Hussain <mohussainan@gmail.com>
2026-09-24 12:55:55 +0530

    feat: update Footer and Player components

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,34 +1,83 @@
-import brandLogo from "../assets/brand-logo.png";
+import brandLogo from "../../public/favicon_io/favicon-32x32.png";
+
 export const Footer = () => {
   return (
-    <footer className="flex flex-col md:flex-row items-center justify-between pt-6 pb-3 px-4 gap-2 md:gap-1">
-      <div className="text-text-primary flex flex-row items-center gap-1 flex-wrap">
-        <div>
-          <img
-            src={brandLogo}
-            alt="Brand Logo"
-            className="md:w-32 w-20 h-auto"
-          />
-        </div>
-        <div>
-          <p className="pt-2 sm:pt-5 text-[0.625rem] sm:text-sm">
-            &copy; 2026 Ain Music. All rights reserved.
-          </p>
-        </div>
+    <footer
+      className="
+        flex
+        flex-col
+        md:flex-row
+        items-center
+        justify-between
+        pt-6
+        pb-3
+        px-4
+        gap-2
+        md:gap-1
+      "
+    >
+      <div
+        className="
+          text-text-primary
+          flex
+          flex-row
+          items-center
+          justify-center
+          gap-3
+          flex-wrap
+        "
+      >
+        <img
+          src={brandLogo}
+          alt="Ain Music"
+          className="w-8 h-8 object-contain"
+        />
+
+        <p
+          className="
+            text-[0.85rem]
+            sm:text-sm
+            text-text-secondary
+          "
+        >
+          &copy; 2026 Ain Music. All rights reserved.
+        </p>
       </div>
 
-      <div className="md:ml-auto text-[0.625rem] sm:text-sm px-1 mt-2 md:mt-0">
-        <p>
-          Designed & developed by{" "}
-          <a
-            href="https://mhansari.shop"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-brand-darkest font-bold  hover:underline"
-          >
-            Mohammed Hussain
-          </a>
-        </p>
+      <div
+        className="
+          md:ml-auto
+          text-sm
+          px-1
+          mt-2
+          md:mt-0
+        "
+      >
+        <span className="text-text-secondary">
+          Designed &amp; developed by{" "}
+        </span>
+
+        <a
+          href="https://hussain-portfolio-pink.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="
+            inline-flex
+            items-center
+            font-bold
+            text-xl
+            hover:underline
+            transition
+          "
+        >
+          <span className="text-[#4285F4]">H</span>
+          <span className="text-[#EA4335]">u</span>
+          <span className="text-[#FBBC05]">s</span>
+          <span className="text-[#FBBC05]">s</span>
+          <span className="text-[#4285F4]">a</span>
+          <span className="text-[#34A853]">i</span>
+          <span className="text-[#EA4335]">n</span>
+        </a>
       </div>
     </footer>
   );
--- frontend/src/components/Player.jsx
+++ frontend/src/components/Player.jsx
@@ -2,7 +2,6 @@ import { useContext, useEffect, useRef, useState } from "react";
 import {
   FiPlay,
   FiPause,
-  FiHeart,
   FiSkipBack,
   FiSkipForward,
   FiShuffle,
@@ -19,15 +18,12 @@ export const Player = ({ song }) => {
     isPlaying,
     setIsPlaying,
     likedSongs,
-    toggleFavorite,
     playlists,
     addSongToPlaylist,
     playNext,
     playPrevious,
-    handleEnded,
     shuffleQueue,
     isShuffled,
-    currentIndex,
   } = useContext(MusicContext);
 
   const audioRef = useRef(null);
@@ -111,26 +107,26 @@ export const Player = ({ song }) => {
   }, [currentSong]);
 
   const titleRef = useRef(null);
-const titleContainerRef = useRef(null);
-const [isTitleOverflowing, setIsTitleOverflowing] = useState(false);
+  const titleContainerRef = useRef(null);
+  const [isTitleOverflowing, setIsTitleOverflowing] = useState(false);
 
-useEffect(() => {
-  const title = titleRef.current;
-  const container = titleContainerRef.current;
+  useEffect(() => {
+    const title = titleRef.current;
+    const container = titleContainerRef.current;
 
-  if (!title || !container) return;
+    if (!title || !container) return;
 
-  const checkOverflow = () => {
-    setIsTitleOverflowing(title.scrollWidth > container.clientWidth);
-  };
+    const checkOverflow = () => {
+      setIsTitleOverflowing(title.scrollWidth > container.clientWidth);
+    };
 
-  checkOverflow();
+    checkOverflow();
 
-  const observer = new ResizeObserver(checkOverflow);
-  observer.observe(container);
+    const observer = new ResizeObserver(checkOverflow);
+    observer.observe(container);
 
-  return () => observer.disconnect();
-}, [currentSong]);
+    return () => observer.disconnect();
+  }, [currentSong]);
 
   const handlePreviousClick = () => {
     const audio = audioRef.current;
@@ -168,103 +164,97 @@ useEffect(() => {
       <audio ref={audioRef} />
       <div
         className="relative
-         w-full
-    m-auto
-    min-h-[88px]
-    min-[648px]:h-[5.625rem]
-    py-2
-    min-[648px]:py-0
-    px-2
-    min-[648px]:px-5
-    bg-surface
-    border-t
-    rounded-t-3xl
-    border-brand-light/40
-    flex
-    flex-col
-    min-[648px]:flex-row
-    items-center
-    justify-center
-    min-[648px]:justify-between
-    gap-2
-    min-[648px]:gap-3
-    z-[950]"
-      >
+        w-full
+        m-auto
+        min-h-[88px]
+        min-[648px]:h-[5.625rem]
+        py-2
+        min-[648px]:py-0
+        px-2
+        min-[648px]:px-5
+        bg-surface
+        border-t
+        rounded-t-3xl
+        border-brand-light/40
+        flex
+        flex-col
+        min-[648px]:flex-row
+        items-center
+        justify-center
+        min-[648px]:justify-between
+        gap-2
+        min-[648px]:gap-3
+        z-[950]"
+          >
         <div
           className="
-    absolute
-    left-0
-    bottom-full
-    mb-2
-    flex
-    items-center
-    gap-2
-    bg-surface/30
-    backdrop-blur
-    rounded-r-xl
-    px-2
-    py-1.5
-    shadow-lg
-    min-w-0
-    min-[648px]:static
-    min-[648px]:mb-0
-    min-[648px]:gap-4
-    min-[648px]:w-auto
-    min-[648px]:min-w-45
+          absolute
+          left-0
+          bottom-full
+          mb-2
+          flex
+          items-center
+          gap-2
+          bg-surface/30
+          backdrop-blur
+          rounded-r-xl
+          px-2
+          py-1.5
+          shadow-lg
+          min-w-0
+          min-[648px]:static
+          min-[648px]:mb-0
+          min-[648px]:gap-4
+          min-[648px]:w-auto
+          min-[648px]:min-w-45
 
-    min-[648px]:border-0
-    min-[648px]:bg-transparent
-    min-[648px]:rounded-none
-    min-[648px]:px-0
-    min-[648px]:py-0
-    min-[648px]:shadow-none
-
-    flex-shrink-0
-  "
-        >
+          min-[648px]:border-0
+          min-[648px]:bg-transparent
+          min-[648px]:rounded-none
+          min-[648px]:px-0
+          min-[648px]:py-0
+          min-[648px]:shadow-none
+          flex-shrink-0
+        "
+              >
           <img
             src={currentSong.coverArt}
             alt={currentSong.title}
             className={`
-    w-10 h-10
-    min-[648px]:w-14 min-[648px]:h-14
-    rounded-full
-    object-cover
-    shadow-sm
-    shrink-0
-    song-cover
-    ${isPlaying ? "song-cover-playing" : ""}
-  `}
+            w-10 h-10
+            min-[648px]:w-14 min-[648px]:h-14
+            rounded-full
+            object-cover
+            shadow-sm
+            shrink-0
+            song-cover
+            ${isPlaying ? "song-cover-playing" : ""}
+          `}
           />
           <div className="flex flex-col justify-center ">
-          <div className="max-w-[160px] min-[648px]:max-w-37.5 overflow-hidden">
-  <p
-    className={`
-      song-title-track
-      text-xs
-      min-[648px]:text-sm
-      text-text-primary
-      font-bold
-
-      ${
-        isPlaying
-          ? "song-title-animated"
-          : "song-title-paused"
-      }
-    `}
-  >
-    {currentSong.title}
-  </p>
-</div>
+            <div className="max-w-[160px] min-[648px]:max-w-37.5 overflow-hidden">
+              <p
+                className={`
+                song-title-track
+                text-xs
+                min-[648px]:text-sm
+                text-text-primary
+                font-bold
+                ${isPlaying ? "song-title-animated" : "song-title-paused"}
+              `}
+              >
+                {currentSong.title}
+              </p>
+            </div>
             <p
               className="
-    text-xs
-    text-text-secondary
-    truncate
-    max-w-[160px]
-    min-[648px]:max-w-37.5
-    cursor-pointer
-    hover:underline"
+              text-xs
+              text-text-secondary
+              truncate
+              max-w-[160px]
+              min-[648px]:max-w-37.5
+              cursor-pointer
+              hover:underline"
             >
               {currentSong.artist}
             </p>
@@ -273,33 +263,36 @@ useEffect(() => {
 
         <div
           className="
-    flex
-    flex-col
-    items-center
-    justify-center
-    w-full
-    min-[648px]:w-[40%]
-    max-w-150
-    gap-2"
+          flex
+          flex-col
+          items-center
+          justify-center
+          w-full
+          min-[648px]:w-[40%]
+          max-w-150
+          gap-2"
         >
           <div
             className="
-    flex
-    items-center
-    justify-center
-    gap-4
-    min-[648px]:gap-6
-    w-full"
+            flex
+            items-center
+            justify-center
+            gap-4
+            min-[648px]:gap-6
+            w-full"
           >
             <FavoriteButton item={currentSong} type="song" />
             <FiShuffle
               onClick={shuffleQueue}
-              className={`cursor-pointer text-lg transition 
-    ${
-      isShuffled
-        ? "text-brand-primary"
-        : "text-text-secondary hover:text-brand-primary"
-    }`}
+              className={`
+                cursor-pointer
+                text-lg
+                transition 
+              ${
+                isShuffled
+                  ? "text-brand-primary"
+                  : "text-text-secondary hover:text-brand-primary"
+              }`}
             />
             <FiSkipBack
               onClick={handlePreviousClick}
@@ -309,20 +302,20 @@ useEffect(() => {
             <button
               onClick={() => setIsPlaying(!isPlaying)}
               className="
-  w-8
-  h-8
-  min-[648px]:w-10
-  min-[648px]:h-10
-  flex
-  items-center
-  justify-center
-  bg-brand-primary
-  text-white
-  rounded-full
-  hover:scale-105
-  hover:bg-brand-dark
-  transition-all
-"
+              w-8
+              h-8
+              min-[648px]:w-10
+              min-[648px]:h-10
+              flex
+              items-center
+              justify-center
+              bg-brand-primary
+              text-white
+              rounded-full
+              hover:scale-105
+              hover:bg-brand-dark
+              transition-all
+            "
             >
               {isPlaying ? (
                 <FiPause className="text-xl" />
@@ -337,36 +330,35 @@ useEffect(() => {
             />
             <FiRepeat className="text-text-secondary hover:text-brand-primary cursor-pointer text-lg transition" />
             <div className="relative">
-            <AddToPlaylistButton onClick={handleAddClick} />
+              <AddToPlaylistButton onClick={handleAddClick} />
 
-            {menuOpen && (
-              <div className="absolute bottom-full mb-2 left-1/2 -translate-x-1/2 bg-surface border border-brand-light/40 rounded-lg shadow-xl w-36 sm:w-44 py-1 max-w-[90vw] z-10">
-                {playlists.map((pl) => (
-                  <button
-                    key={pl._id}
-                    onClick={() => handlePlaylistSelect(pl._id)}
-                    className="block w-full text-left px-3 py-2 text-sm text-text-secondary hover:bg-brand-light/20 hover:text-text-primary"
-                  >
-                    {pl.name}
-                  </button>
-                ))}
-              </div>
-            )}
-          </div>
+              {menuOpen && (
+                <div className="absolute bottom-full mb-2 left-1/2 -translate-x-1/2 bg-surface border border-brand-light/40 rounded-lg shadow-xl w-36 sm:w-44 py-1 max-w-[90vw] z-10">
+                  {playlists.map((pl) => (
+                    <button
+                      key={pl._id}
+                      onClick={() => handlePlaylistSelect(pl._id)}
+                      className="block w-full text-left px-3 py-2 text-sm text-text-secondary hover:bg-brand-light/20 hover:text-text-primary"
+                    >
+                      {pl.name}
+                    </button>
+                  ))}
+                </div>
+              )}
+            </div>
           </div>
 
           <div className="flex items-center gap-2 w-full ">
             <span
               className="
-    text-[10px]
-    min-[648px]:text-xs
-    text-text-secondary
-    w-6
-    min-[648px]:w-10
-    shrink-0
-    text-right
-    font-medium
-  "
+              text-[10px]
+              min-[648px]:text-xs
+              text-text-secondary
+              w-6
+              min-[648px]:w-10
+              shrink-0
+              text-right
+              font-medium"
             >
               {formatTime(progress)}
             </span>
@@ -380,13 +372,13 @@ useEffect(() => {
             />
             <span
               className="
-    text-[10px]
-    min-[648px]:text-xs
-    text-text-secondary
-    w-10
-    text-left
-    font-medium
-  "
+              text-[10px]
+              min-[648px]:text-xs
+              text-text-secondary
+              w-10
+              text-left
+              font-medium
+            "
             >
               {formatTime(duration)}
             </span>
@@ -395,28 +387,20 @@ useEffect(() => {
 
         <div
           className="
-    flex
-    justify-center
-    items-center
-
-    gap-3
-    min-[648px]:gap-5
-
-    pr-1
-    min-[648px]:pr-5
-
-    shrink-0
-
-    absolute
-    top-3
-    right-2
-
-    min-[648px]:static
-  "
-        >
-          
-          
-        </div>
+          flex
+          justify-center
+          items-center
+          gap-3
+          min-[648px]:gap-5
+          pr-1
+          min-[648px]:pr-5
+          shrink-0
+          absolute
+          top-3
+          right-2
+          min-[648px]:static
+        "
+        ></div>
       </div>
     </>
   );
